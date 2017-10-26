8ba4125a7c008c21fce3f3f718b4827ab1bb8b1d
Stephen Wright <sjw948@gmail.com>
2017-10-26 08:47:06 +0100

    simple-pig-latin challenge complete

---
+++ 5-kyu/simple-pig-latin.js
@@ -0,0 +1,23 @@
+/*
+
+Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+
+*/
+
+function pigIt(str){
+  const strArray = str.split(' ')
+  pigLatinArray = []
+
+  strArray.forEach(function (string) {
+    let firstLetter = string[0]
+    string += `${firstLetter}ay`
+    string = string.slice(1, string.length)
+    pigLatinArray.push(string)
+  })
+
+  return pigLatinArray.join(', ').replace(/,/g, '')
+}
+
+pigIt('This is my string')
