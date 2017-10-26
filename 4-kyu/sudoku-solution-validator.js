4c068516923b09416464c2695ef2b1eefbdfe443
Stephen Wright <sjw948@gmail.com>
2017-10-26 22:21:13 +0100

    sudoku solution validator challenge completed

---
+++ 4-kyu/sudoku-solution-validator.js
@@ -0,0 +1,96 @@
+/*
+
+Sudoku Background
+
+Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+(More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+Sudoku Solution Validator
+
+Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+Examples
+
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]); // => true
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]); // => false
+
+*/
+
+function validSolution(board){
+
+  const clonedArray = JSON.parse(JSON.stringify(board))
+
+  for (let i = 0; i < 9; i += 1) {
+    let tempBoard = JSON.parse(JSON.stringify(board));
+    if (tempBoard[i].sort().join('') != 123456789) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i += 1) {
+    let tempBoard = JSON.parse(JSON.stringify(board))
+    let tempGrid = []
+    for (let j = 0; j < 9; j += 1) {
+      tempGrid.push(board[j][i])
+    }
+
+    if (tempGrid.sort().join('') != 123456789) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i += 3) {
+    let tempBoard = JSON.parse(JSON.stringify(board))
+    for (let j = 0; j < 9; j += 3) {
+      let tempGrid = []
+      tempGrid.push(board[i][j])
+      tempGrid.push(board[i][j + 1])
+      tempGrid.push(board[i][j + 2])
+      tempGrid.push(board[i + 1][j])
+      tempGrid.push(board[i + 1][j + 1])
+      tempGrid.push(board[i + 1][j + 2])
+      tempGrid.push(board[i + 2][j])
+      tempGrid.push(board[i + 2][j + 1])
+      tempGrid.push(board[i + 2][j + 2])
+      if (tempGrid.sort().join('') != 123456789) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+])
