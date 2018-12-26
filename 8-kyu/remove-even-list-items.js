ebc5a2da8a43860f9ed0b5a4eca185709622a4b5
Stephen Wright <sjw948@gmail.com>
2018-12-26 13:25:28 +0000

    remove even list items challenge

---
+++ 8-kyu/remove-even-list-items.js
@@ -0,0 +1,14 @@
+/* Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+
+Example: arr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+  const newArr = [];
+  for (let i = 0; i < arr.length; i+= 2) {
+    newArr.push(arr[i])
+  }
+
+  return newArr;
+}
