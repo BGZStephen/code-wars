f204b4b357a75966ab40c56c870a3b9dc4a8b847
Stephen Wright <sjw948@gmail.com>
2018-12-26 12:09:06 +0000

    remove exclamation marks test

---
+++ 8-kyu/remove-exclamation-marks.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(string) {
+  return string.replace(new RegExp(/!/, 'g'), '');
+}
+
+console.log(removeExclamationMarks('!1111!111!!'))
