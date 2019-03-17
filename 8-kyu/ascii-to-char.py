e0e53ef2207cd589df534600edf4e59c554bd9b0
stephen <sjw948@gmail.com>
2019-03-17 14:02:37 +0000

    ascii to char in python / js

---
+++ 8-kyu/ascii-to-char.py
@@ -0,0 +1,7 @@
+# Get character from ASCII Value
+# Write a function getChar/GetChar/get_char which takes a number and returns the corresponding ASCII char for that value.
+
+def getChar(c):
+  return chr(c)
+
+print(getChar(65))
