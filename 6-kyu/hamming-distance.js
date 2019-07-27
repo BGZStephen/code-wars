045804e0967ef8c92d0c87370c2a34c03da70489
Stephen Wright <sjw948@gmail.com>
2019-07-27 11:56:11 +0100

    hamming distance code submission

---
+++ 6-kyu/hamming-distance.js
@@ -0,0 +1,13 @@
+// The Hamming Distance is a measure of similarity between two strings of equal length. Complete the function so that it returns the number of positions where the input strings do not match.
+
+function hamming(a,b) {
+  let mismatches = 0;
+  
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      mismatches += 1; 
+    }
+  }
+
+  return mismatches;
+}
