6aa4779e51818b79ece8aee9e826b9db36b10c82
Stephen Wright <sjw948@gmail.com>
2020-04-26 10:56:27 +0100

    permutations solution

--- 4-kyu/permutations.js
+++ 4-kyu/permutations.js
@@ -7,3 +7,27 @@
 // permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 // The order of the permutations doesn't matter.
 
+const permutations = (string) => {
+  if (!string || typeof string !== "string") {
+    return []
+  } else if (string.length < 2 ){
+    return [string]
+  }
+
+  const permutationsArray = [] 
+   
+  for (let i = 0; i < string.length; i++){
+    let char = string[i]
+
+    if (string.indexOf(char) !== i) 
+    continue
+
+    let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
+
+    for (let permutation of permutations(remainingChars)){
+      permutationsArray.push(char + permutation) 
+    }
+  }
+
+  return permutationsArray
+}
