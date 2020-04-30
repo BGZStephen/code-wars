8f2f2b9f08abab1d7821f6515a47a913498e86a3
Stephen Wright <sjw948@gmail.com>
2020-04-30 14:55:37 +0100

    i need more speed solution

--- 6-kyu/i-need-more-speed.js
+++ 6-kyu/i-need-more-speed.js
@@ -7,3 +7,21 @@
 // Array should be reversed in place! (no need to return it)
 // Usual builtins have been deactivated. Don't count on them.
 // You'll have to do it fast enough, so think about performances
+
+function reverse(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    const tempA = arr[start]
+    const tempB = arr[end]
+
+    arr[end] = tempA
+    arr[start] = tempB
+
+    start ++;
+    end --;
+  }
+
+  return arr;
+}
