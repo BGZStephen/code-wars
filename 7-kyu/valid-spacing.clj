660572cb2fd43e16fe2c38b8e2f8fe745c0bd65e
Stephen <sjw948@gmail.com>
2020-12-12 08:55:49 +0000

    is valid spacing to clojure

---
+++ 7-kyu/valid-spacing.clj
@@ -0,0 +1,26 @@
+;; Task
+;; Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
+
+;; For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+
+;; 'Hello world' = true
+;; ' Hello world' = false
+;; 'Hello world  ' = false
+;; 'Hello  world' = false
+;; 'Hello' = true
+;; // Even though there are no spaces, it is still valid because none are needed
+;; 'Helloworld' = true 
+;; // true because we are not checking for the validity of words.
+;; ' ' = false
+;; '' = true
+;; Note - there will be no punctuation or digits in the input string, only letters.
+
+(ns kata)
+
+(defn is-valid-spacing? [s]
+  (cond
+    (re-find #"^ " s) false
+    (re-find #" $" s) false
+    (re-find #" + " s) false
+    :else true)
+  )
