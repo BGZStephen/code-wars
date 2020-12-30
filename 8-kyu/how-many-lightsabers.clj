04be54785366649828b00a0fe7a323891d633034
Stephen <sjw948@gmail.com>
2020-12-30 15:14:18 +0000

    how many lightsabers clojure

---
+++ 8-kyu/how-many-lightsabers.clj
@@ -0,0 +1,14 @@
+;; Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn/how_many_light_sabers_do_you_own that
+
+;; accepts the name of a programmer, and
+;; returns the number of lightsabers owned by that person.
+;; The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+;; (howManyLightsabersDoYouOwn "Anny Wan-Else")     == 0
+;; (howManyLightsabersDoYouOwn "Hugh R. Dareader")  == 0
+;; (howManyLightsabersDoYouOwn "Zach")              == 18
+
+(ns lightsabers)
+
+(defn howManyLightsabersDoYouOwn [name]
+  (if (= name "Zach") 18 0))
