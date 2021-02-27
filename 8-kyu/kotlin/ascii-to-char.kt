8dc234c31504505c4a433078333b1672a5e56769
Stephen <sjw948@gmail.com>
2021-02-27 09:49:46 +0000

    ascii to char kotlin

---
+++ 8-kyu/kotlin/ascii-to-char.kt
@@ -0,0 +1,6 @@
+// Get character from ASCII Value
+// Write a function getChar/GetChar/get_char which takes a number and returns the corresponding ASCII char for that value.
+
+fun getChar(c: Int): Char {
+  return c.toChar();
+}
