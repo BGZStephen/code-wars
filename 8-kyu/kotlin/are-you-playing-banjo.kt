8b9aa26de00d67f06f60b063078a9dff3b7abce9
Stephen <sjw948@gmail.com>
2021-03-04 12:29:52 +0000

    refactor inline

--- 8-kyu/kotlin/are-you-playing-banjo.kt
+++ 8-kyu/kotlin/are-you-playing-banjo.kt
@@ -1,6 +1,5 @@
 object AreYouPlayingBanjo {
   fun areYouPlayingBanjo(name:String):String {
-    var beginsWithR = name[0] == 'R' || name[0] == 'r'
-    return if (beginsWithR) "$name plays banjo" else "$name does not play banjo"
+    return if (name[0] == 'R' || name[0] == 'r') "$name plays banjo" else "$name does not play banjo"
   }
 }
