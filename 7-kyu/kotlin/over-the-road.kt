69b54349de2909d9895217bd9ec541c92ebc8b8d
Stephen <sjw948@gmail.com>
2021-03-09 08:22:45 +0000

    performance improvements

--- 7-kyu/kotlin/over-the-road.kt
+++ 7-kyu/kotlin/over-the-road.kt
@@ -1,6 +1,6 @@
 fun overTheRoad(address: Int, n: Int): Int {
   val left = (1..(n * 2)).toList().filter {value -> value % 2 != 0}
-  val right = left.map {value -> value + 1}.reversed()
+  val right = (1..(n * 2)).toList().reversed().filter {value -> value % 2 == 0}
 
   return if (address % 2 == 0) {
     left[right.indexOf(address)]
