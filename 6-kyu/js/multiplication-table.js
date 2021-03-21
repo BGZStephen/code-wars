1daa4be2ed4fdf8b598cd76f93d2347660ce1651
Stephen <sjw948@gmail.com>
2021-03-21 10:50:54 +0000

    multiplication table solution

--- 6-kyu/js/multiplication-table.js
+++ 6-kyu/js/multiplication-table.js
@@ -6,3 +6,18 @@
 // 2 4 6
 // 3 6 9
 // for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+function multiplicationTable(size) {
+  const res = [];
+  for (let i = 1; i <= size; i++) {
+    res.push([i])
+  }
+
+  for (let i = 0; i < res.length; i++) {
+    for (let j = 2; j <= size; j++) {
+      res[i].push(j * res[i][0])
+    }
+  }
+
+  return res;
+}
