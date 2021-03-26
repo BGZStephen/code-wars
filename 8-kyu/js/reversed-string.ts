6cc374f1d3aa18f850b3e77a66f06212b1ff576c
Stephen <sjw948@gmail.com>
2021-03-26 12:51:44 +0000

    reverse string solution

---
+++ 8-kyu/js/reversed-string.ts
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+
+export function solution(str: string): string {
+  return str.split("").reverse().join("")
+}
