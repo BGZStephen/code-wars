02fda4fed7f4ad921ce59f7d48c49925875aa6b2
Stephen <sjw948@gmail.com>
2021-09-07 14:42:23 +0100

    integer difference solution

--- 7-kyu/js/integer-difference.js
+++ 7-kyu/js/integer-difference.js
@@ -7,3 +7,28 @@
 // [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
 // [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
 
+const intDiff = (arr, n) => {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        if (arr[i] === arr[j] && n === 0) {
+          count += 1;
+        }
+
+        if (arr[i] < arr[j] && arr[j] - n === arr[i]) {
+          count += 1;
+        }
+
+        if (arr[j] < arr[i] && arr[i] - n === arr[j]) {
+          count += 1;
+        }
+      }
+    } 
+  }
+
+  return count / 2;
+}
+
+intDiff([1, 1, 3, 3], 2)
