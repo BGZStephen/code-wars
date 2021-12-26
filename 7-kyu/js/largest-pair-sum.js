81433504e294e91e94d1d604b412340bf4df3bb4
Stephen <sjw948@gmail.com>
2021-12-26 08:13:46 +0000

    largest pair sum solution

--- 7-kyu/js/largest-pair-sum.js
+++ 7-kyu/js/largest-pair-sum.js
@@ -5,3 +5,9 @@
 // [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 // [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 // Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers) {
+  const reverseSortedNumbers = numbers.sort((a, b) => b - a);
+  
+  return reverseSortedNumbers[0] + reverseSortedNumbers[1]
+}
