45c09480e66726aa2518c3ecfc9e623260240d9f
Stephen <sjw948@gmail.com>
2021-12-27 11:28:28 +0000

    refined

--- 7-kyu/js/a-gift-well-spent.js
+++ 7-kyu/js/a-gift-well-spent.js
@@ -15,7 +15,7 @@
 
 var buy = function(x, arr){
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j <= arr.length - 1; j++) {
+    for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === x) {
         return [i, j]
       }
