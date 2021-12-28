6ae7beb1e8c8ff83ad8ed2e06d232f6c7f7d7f98
Stephen <sjw948@gmail.com>
2021-12-28 10:14:06 +0000

    Rearrange Number to Get its Maximum challenge

---
+++ 7-kyu/js/rearrange-number-to-get-max.js
@@ -0,0 +1,6 @@
+// Create a function that takes one positive three digit integer and rearranges its digits to get the maximum possible number. Assume that the argument is an integer.
+
+// Return `nil` if the argument is not valid.
+// Returm null if the argument is invalid.
+
+// maxRedigit(123) --> 321
