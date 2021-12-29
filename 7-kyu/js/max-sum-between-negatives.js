db96bb8e590b742f0aac9837eec881b6e0ff5e3a
Stephen <sjw948@gmail.com>
2021-12-29 09:48:06 +0000

    Max sum between two negatives challenge

---
+++ 7-kyu/js/max-sum-between-negatives.js
@@ -0,0 +1,9 @@
+// You have a list of integers. The task is to return the maximum sum of the elements located between two negative elements, or if there is no such sum: -1 for Python, JavaScript and COBOL, Nothing for Haskell.
+
+// No negative element should be present in this sum.
+
+// Example:
+
+// [4, -1, 6, -2, 3, 5, -7, 7] -> 8
+//      ^      ^         ^
+// Not 14, because -2 is between -1 and -7, and not 6 but 8, because 8 is bigger.
