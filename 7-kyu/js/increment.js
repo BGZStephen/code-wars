d87f7c68e34678f0da8a44bd2bdc97b57e09e2e3
Stephen <sjw948@gmail.com>
2021-12-30 09:55:00 +0000

    increment solution

--- 7-kyu/js/increment.js
+++ 7-kyu/js/increment.js
@@ -10,3 +10,15 @@
 
 // [4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
 //                                        #  9+3 = 12  -->  2
+
+function incrementer(nums) { 
+  return nums.map((number, index) => {
+    let res = number + index + 1;
+    
+    while (res >= 10) {
+      res -= 10;
+    }
+    
+    return res;
+  })
+}
