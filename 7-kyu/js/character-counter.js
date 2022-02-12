213dedd64805df48f16ee1a9a6f3b23344631dc4
Stephen <sjw948@gmail.com>
2022-02-12 09:07:12 +0000

    character counter solution

--- 7-kyu/js/character-counter.js
+++ 7-kyu/js/character-counter.js
@@ -8,3 +8,17 @@
 // "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
 // "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
 // "123abc!" is a valid word because all of the characters only appear once in the word.
+
+function validateWord(s) {
+  const map = {};
+
+  for (const letter of s.toLowerCase()) {
+    if (map[letter]) {
+      map[letter] += 1
+    } else {
+      map[letter] = 1
+    }
+  }
+
+  return Object.values(map).every((val, index, arr) => val === arr[0])
+}
