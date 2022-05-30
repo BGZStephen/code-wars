2b17612dff5364c51dc24183c9b3c15400efa5ff
Stephen <sjw948@gmail.com>
2022-05-30 19:27:33 +0100

    freudian-translator solution

--- 8-kyu/js/freudian-translator.js
+++ 8-kyu/js/freudian-translator.js
@@ -6,3 +6,10 @@
 
 // In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string.
 
+function toFreud(string) {
+    if (!string) {
+        return "";
+    }
+
+    return Array(string.split(" ").length).fill("sex").join(" ")
+}
