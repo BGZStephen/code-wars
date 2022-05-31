5e5a3960f4b78f74dfde6abfe7d64166367f29f4
Stephen <sjw948@gmail.com>
2022-05-31 10:29:50 +0100

    name on billboard solution

--- 8-kyu/js/name-on-billboard.js
+++ 8-kyu/js/name-on-billboard.js
@@ -3,3 +3,7 @@
 // You can not use multiplier "*" operator.
 
 // If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+
+function billboard(name, price = 30) {
+    return name.split("").reduce((acc, _) => acc + price, 0)
+} 
