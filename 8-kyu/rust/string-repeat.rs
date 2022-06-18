9d030932c2442035cdfe61bcc275181d3ed0f023
Stephen <sjw948@gmail.com>
2022-06-18 10:54:05 +0100

    string repeat challenge

--- 8-kyu/rust/string-repeat.rs
+++ 8-kyu/rust/string-repeat.rs
@@ -1,3 +1,9 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
 fn repeat_str(src: &str, count: usize) -> String {
   let mut res = String::new();
     
