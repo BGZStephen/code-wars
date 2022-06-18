8e21a2c22d1961600bb2a94b7247fd27b19f2f81
Stephen <sjw948@gmail.com>
2022-06-18 10:52:17 +0100

    count-odd-numbers-below-n solution

--- 8-kyu/rust/count-odd-numbers-below-n.rs
+++ 8-kyu/rust/count-odd-numbers-below-n.rs
@@ -3,3 +3,15 @@
 // Examples (Input -> Output)
 // 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+fn odd_count(n: u64) -> u64 {
+  let mut res: u64 = 0;
+  let mut iterator: u64 = 1;
+  
+  while iterator < n {
+    res += 1;
+    iterator += 2;
+  }
+  
+  return res;
+}
