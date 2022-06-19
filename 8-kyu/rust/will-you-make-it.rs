7792b4f35a4459e82202ec9c1fa94605d8ca85fc
Stephen <sjw948@gmail.com>
2022-06-19 08:00:09 +0100

    will-you-make-it solution

--- 8-kyu/rust/will-you-make-it.rs
+++ 8-kyu/rust/will-you-make-it.rs
@@ -3,3 +3,7 @@
 // Considering these factors, write a function that tells you if it is possible to get to the pump or not.
 
 // Function should return true if it is possible and false if not.
+
+fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
+  return mpg * gallons >= distance_to_pump;
+}
