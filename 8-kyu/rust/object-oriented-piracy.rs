43cf147e9d1f2ace4cd529da946f95a2b56b4cd4
Stephen <sjw948@gmail.com>
2022-06-19 08:15:08 +0100

    object-oriented-piracy solution

--- 8-kyu/rust/object-oriented-piracy.rs
+++ 8-kyu/rust/object-oriented-piracy.rs
@@ -30,3 +30,13 @@
 
 // Good luck!
 
+struct Ship {
+  draft: u32,
+  crew: u32,
+}
+
+impl Ship {
+  fn is_worth_it(&self) -> bool {
+    return self.draft as f32 - (self.crew as f32 * 1.5) > 20 as f32;
+  } 
+}
