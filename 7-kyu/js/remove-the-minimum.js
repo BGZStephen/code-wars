43b6da772a1465489826d84d0949d134e32b0eb7
Stephen <sjw948@gmail.com>
2022-07-02 09:36:05 +0100

    remove the minimum solution

--- 7-kyu/js/remove-the-minimum.js
+++ 7-kyu/js/remove-the-minimum.js
@@ -12,3 +12,20 @@
 // * Input: [1,2,3,4,5], output= [2,3,4,5]
 // * Input: [5,3,2,1,4], output = [5,3,2,4]
 // * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+  let removed = false;
+  const smallest = Math.min(...numbers);
+  let res = [];
+
+  for (const number of numbers) {
+    if (number === smallest && !removed) {
+      removed = true;
+      continue;
+    }
+
+    res.push(number);
+  }
+
+  return res;
+}
