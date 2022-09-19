dc48ac866bfc6894e3a8d3778b4d5ea4271bb55d
Stephen <sjw948@gmail.com>
2022-09-19 10:48:12 +0100

    switch-statements solutions

--- 7-kyu/js/switch-statements.js
+++ 7-kyu/js/switch-statements.js
@@ -64,3 +64,24 @@
 // Tip: Using default for most of the cases can reduce your work.
 
 // When you have finished, click "Test" to test your code against initial tests and "Attempt" to test your code against all tests. If you pass in all tests you can click "Submit" to submit your code.
+
+function howManydays(month){
+  
+  switch (month){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    case 2:
+      return 28;
+  }
+}
