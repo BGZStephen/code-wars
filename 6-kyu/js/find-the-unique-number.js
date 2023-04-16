cb5fda8f17d0ee0c354021fde8679e74d783d3c4
Stephen <sjw948@gmail.com>
2023-04-16 08:08:03 +0100

    Find the unique number solution

--- 6-kyu/js/find-the-unique-number.js
+++ 6-kyu/js/find-the-unique-number.js
@@ -11,3 +11,21 @@
 // Find the unique number (this kata)
 // Find the unique string
 // Find The Unique
+
+function findUniq(arr) {
+  const countHashMap = {};
+
+  for (const number of arr) {
+    if (countHashMap[number] === undefined) {
+      countHashMap[number] = 0;
+    }
+
+    countHashMap[number] += 1;
+  }
+
+  for (const [key, value] of Object.entries(countHashMap)) {
+    if (value === 1) {
+      return parseFloat(key);
+    }
+  }
+}
