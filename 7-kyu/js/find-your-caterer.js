ca989eaa0ea8a7ee00c4aff1dd507b2b281a45b7
Stephen <sjw948@gmail.com>
2023-05-04 19:44:16 +0100

    find your caterer solution

--- 7-kyu/js/find-your-caterer.js
+++ 7-kyu/js/find-your-caterer.js
@@ -11,3 +11,29 @@
 
 // budget, people => (300, 9) will return 3 and so on.
 // Goodluck :)
+
+function findCaterer(budget, people){
+  console.log(budget, people)
+  
+  if (people == 0) {
+    return -1
+  }
+  
+  if (people > 60 && people * (30 * .8) <= budget) {
+    return 3
+  }
+  
+  if (people * 30 <= budget) {
+    return 3
+  }
+  
+  if (people * 20 <= budget) {
+    return 2
+  }
+  
+  if (people * 15 <= budget) {
+    return 1
+  }
+  
+  return -1
+}
